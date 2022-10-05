9f43185c7dddcf347d212032fecdb269be75bd9f
KyrellosSaleeb <87317770+KyrellosSaleeb@users.noreply.github.com>
2022-10-06 01:48:51 +0200

    Add files via upload

--- STD_TYPES.h
+++ STD_TYPES.h
@@ -1,21 +1,22 @@
-#ifndef STD_TYPES_H_
-#define STD_TYPES_H_
+/**********************************************************************************************/
+/***** Name: Kyrellos Saleeb             ********************************************************/
+/***** Component: Standard Types         ********************************************************/
+/**********************************************************************************************/
 
-typedef unsigned char u8;
-typedef signed char s8;
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
 
-typedef unsigned short int u16;
-typedef signed short int s16;
+typedef unsigned char 		u8	;
+typedef unsigned short 		u16	;
+typedef unsigned int 		u32	;
+typedef unsigned long 		u64	;
 
-typedef unsigned long int u32;
-typedef signed long int s32;
+typedef signed char 		s8	;
+typedef signed short 		s16	;
+typedef signed int	 		s32	;
 
-typedef unsigned long long int u64;
-typedef signed long long int s64;
-
-typedef float f32;
-typedef double f64;
-typedef long double f96;
-
+typedef float				f32	;
+typedef double 				f64	;
+typedef long double 		f96	;
 
 #endif
